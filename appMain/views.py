2a6f1eae002034f9c36011317ec55a02633fcbde
httpsGabrielle <gabrielleso125@gmail.com>
2022-09-16 18:24:09 -0300

    add login

---
+++ appMain/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Videos, Games, Atividades
+
+def base(request):
+    return render(request, 'base.html')
+    
+def home(request): 
+    videos = Videos.objects.all()
+    return render(request, 'home.html')
+
+def tarefas(request): 
+    return render(request, 'tarefas.html')
+
+def videos(request): 
+    videos = Videos.objects.all()
+    return render(request,'videosroom.html',{'videos':videos})
+
+def games(request): 
+    games = Games.objects.all()
+    return render(request,'gameroom.html',{'games':games})
+
+def atividades(request): 
+    atividades = Atividades.objects.all()
+    return render(request,'taskroom.html',{'atividades':atividades})
---
+++ educaItu/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from appMain import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('tarefas/', views.tarefas, name='tarefas'),
+    path('videos/', views.videos, name='videos'),
+    path('games/', views.games, name='games'),
+    path('atividades/', views.atividades, name='atividades'),
+    path('accounts/', include('django.contrib.auth.urls')),
+]
